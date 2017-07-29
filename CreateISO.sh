20630d852de41c9f226048a451705dc54a9442df
tazboyz16 <tazboyz_16@yahoo.com>
2017-07-29 16:04:25 -0400

    Update CreateISO.sh

--- CreateISO.sh
+++ CreateISO.sh
@@ -19,7 +19,7 @@ rm -r /opt/serveriso
 rm -r /opt/Ubuntu-Server-Auto-Install
 #rm /opt/ubuntu-*.iso
 
-apt install git-core
+apt install git-core genisoimage
 clear
 echo "Fully Automated Script to Download Your Ubuntu ISO, "
 echo "Unpack it, edit the MyApps Scripts and then ReImage the ISO back together for you"
